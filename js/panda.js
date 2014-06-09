bc7e5858c9cabf032c9212147bc75cd186480561
lindachan1022 <linlinchan04@qq.com>
2014-06-09 10:27:52 +0800

    index&login
    index&login

---
+++ js/panda.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+//case
+
+$(window).load(function(){
+
+    $('[data-zlname = test]').mateHover({
+        position: 'y-reverse',
+        overlayStyle: 'classic',
+        overlayOpacity: 0.7,
+        popupEasing: 'easeOutQuint'
+    });
+
+});
+
+
+
+
+
+
+
+// percentbox
+$( document ).ready(function() {
+    $('.myStathalf2').circliful();
+});
+
+// map
+function map()
+{
+    $("#map").click();
+}
+
+
+
+
+// case
+function caseMore()
+{
+    $("#casemore").click();
+}
